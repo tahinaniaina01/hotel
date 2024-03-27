2d705d980d693c950fda19ed713ddd9ae37261af
Tahinaniaina <tahinaniaina02@gmail.com>
2024-03-28 02:31:35 +0300

    page structuring

--- src/App.tsx
+++ src/App.tsx
@@ -1,12 +1,24 @@
+import { createBrowserRouter, RouterProvider } from "react-router-dom";
+import Home from "./pages/Home";
+import RoomsDetails from "./pages/RoomsDetails";
 
+const router = createBrowserRouter([
+  {
+    path: "/",
+    element: <Home />,
+  },
+  {
+    path: "/:room",
+    element: <RoomsDetails />,
+  },
+]);
 
 function App() {
-
   return (
     <>
-      
+      <RouterProvider router={router} />
     </>
-  )
+  );
 }
 
-export default App
+export default App;
